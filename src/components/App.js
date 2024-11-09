577fd9e19eab045aa1c95c62a07f7cb1f0fed9a5
florence <loracodes3@gmail.com>
2024-11-10 01:34:44 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,12 @@ import About from "./About";
 import imageUrl from "./images/demo.png"
 
 function App() {
+  const username ="Liz"
+  const city ="Newyork"
   return (
     <div>
       <NavBar />
-      <Home />
+      <Home username={username} city={city}/>
       <About image ={imageUrl}/>
     </div>
   );
