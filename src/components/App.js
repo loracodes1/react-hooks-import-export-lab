eb5b77775b2aac77b7e44836a5a419dd3ebae704
florence <loracodes3@gmail.com>
2024-11-10 01:54:01 +0300

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import Home from "./Home"
 import NavBar from "./NavBar";
 import About from "./About";
-import imageUrl from "./images/demo.png"
+import const username ="Liza" from "./data/user"
 
 function App() {
-  const username ="Liz"
-  const city ="Newyork"
   return (
     <div>
       <NavBar />
       <Home username={username} city={city}/>
-      <About image ={imageUrl}/>
+      <About />
     </div>
   );
 }
--- src/data/user.js
+++ src/data/user.js
@@ -4,3 +4,7 @@ const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+ export username ="Liza";
+ export city = "New york "
+ export image = "https://i.imgur.com/mV8PQxj.gif";
+
